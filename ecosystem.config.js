35f05e91e827e852e33e052dcb55ec3adb48032c
Kassio <kassio150@gmail.com>
2026-04-30 12:12:05 -0300

    Add log capture to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
     script: 'npm',
     args: 'start',
     cwd: '/home/rifasonline/htdocs/whatsrifas.com.br/rifasonline',
+    error_file: '/home/rifasonline/htdocs/whatsrifas.com.br/rifasonline/logs/error.log',
+    out_file: '/home/rifasonline/htdocs/whatsrifas.com.br/rifasonline/logs/out.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
